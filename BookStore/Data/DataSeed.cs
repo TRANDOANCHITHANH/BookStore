b2db621615b56bf854a747e383c029d0b6b20d52
TRANDOANCHITHANH <tranchithanh0404@gmail.com>
2024-10-10 02:06:38 +0700

    .

--- BookStore/Data/DataSeed.cs
+++ BookStore/Data/DataSeed.cs
@@ -1,46 +1,4 @@
-﻿/*using BookStore.Authorization;
-using Microsoft.AspNetCore.Identity;
-
-namespace BookStore.Data
-{
-	public class DataSeed
-	{
-		public static async Task InitData(IServiceProvider service)
-		{
-			var mnUser = service.GetService<UserManager<IdentityUser>>();
-			var mnRole = service.GetService<RoleManager<IdentityRole>>();
-			//Them vai tro
-			await mnRole.CreateAsync(new IdentityRole(PhanQuyen.Admin.ToString()));
-			await mnRole.CreateAsync(new IdentityRole(PhanQuyen.User.ToString()));
-
-			//Tao thong tin mac dinh cho acc admin
-			var ad = new IdentityUser
-			{
-				UserName = "admin@gmail.com",
-				Email = "admin@gmail.com",
-				EmailConfirmed = true
-			};
-			var userDb = await mnUser.FindByEmailAsync(ad.Email);
-			if(userDb is null)
-			{
-				var ketqua = await mnUser.CreateAsync(ad, "Admin12345@");
-				if(ketqua.Succeeded) {
-				await mnUser.AddToRoleAsync(ad,PhanQuyen.Admin.ToString());
-				}
-			else
-				{
-					foreach (var loi in ketqua.Errors)
-					{
-						Console.WriteLine(loi.Description);
-					}
-				}
-				
-			}
-			
-		}
-	}
-}
-*/
+﻿
 using Microsoft.AspNetCore.Identity;
 using BookStore.Const;
 using System.Data;
@@ -55,12 +13,11 @@ namespace BookStore.Data
 			var quanLyNguoiDung = dichVu.GetService<UserManager<IdentityUser>>();
 			var quanLyVaiTro = dichVu.GetService<RoleManager<IdentityRole>>();
 
-			// Thêm một vài trò thêm một role vào cơ sở dữ liệu
+			
 			await quanLyVaiTro.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
 			await quanLyVaiTro.CreateAsync(new IdentityRole(Roles.User.ToString()));
 
-			// Tạo thông tin mặc định cho tài khoản Admin,
-			// bao gồm: UserName, Email, xác thực Email
+			
 			var quanTri = new IdentityUser
 			{
 				UserName = "admin@test.xyz",
@@ -70,8 +27,7 @@ namespace BookStore.Data
 
 			var nguoiDungTrongCsdl = await quanLyNguoiDung.FindByEmailAsync(quanTri.Email);
 
-			/// Nếu tài khoản của Admin không tồn tại trong cơ sở dữ liệu
-			/// hoặc có thể hiểu là chưa có trong csdl
+			
 			if (nguoiDungTrongCsdl is null)
 			{
 				// Tạo tài khoản Admin với mật khẩu là Admin12345!@#$%
